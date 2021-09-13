a40f29cc731826e7dc5473df82c6dc77366695be
Maria <maria.mihaila.17@gmail.com>
2021-09-13 12:29:09 +0300

    Add get-balance integration test

---
+++ src/get-balance.int.test.js
@@ -0,0 +1,36 @@
+import {expect} from 'chai';
+import request from 'supertest';
+import {createWebServer} from "./create-web-server.js";
+
+
+describe('/balance/:personId', () => {
+    let port;
+    let server;
+
+    before(async () => {
+        server = createWebServer();
+        port = server.port;
+
+        await server.start();
+    });
+
+    after(async () => {
+        await server.stop();
+    });
+
+    it('given existing balance > when calling get /balance/:personId > should return valid response', async () => {
+        const payload = {
+            requestId: 1,
+            username: 'person123',
+            externalToken: 1
+        };
+
+        const response = await request(`http://localhost:${port}`).get('/balance/1').send(payload);
+
+        expect(response.status).to.be.equal(200);
+        expect(response.body).to.be.deep.equal({
+            personId: "1",
+            balance: "2000"
+        });
+    });
+});
