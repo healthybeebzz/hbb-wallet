08cf26416159a5987b09b8348eb9ea2052ec6cf0
Maria <maria.mihaila.17@gmail.com>
2021-09-14 12:28:03 +0300

    Create error handler

---
+++ src/errorHandler.js
@@ -0,0 +1,7 @@
+/*
+    Catches and logs all errors. Returns 500 and a general message to the user.
+ */
+export const errorHandler = (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send('Oops, something went wrong!');
+}
