7419fc493ca70ba1e0e6d33d05bda58ae99a23f1
Maria <maria.mihaila.17@gmail.com>
2021-09-14 12:56:25 +0300

    Replace hardcoded values with env variables

--- src/db-connection.js
+++ src/db-connection.js
@@ -1,14 +1,13 @@
 // Establish connection to db
 import pg from "pg";
 
-
 export const connectToDb = () =>  {
     const pool = new pg.Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'postgres',
-        password: 'postgres',
-        port: 5432
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT
     })
     return pool;
 }
