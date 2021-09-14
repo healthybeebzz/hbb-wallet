33423f13c728fd9ea037fc4a9f950fc01bb8679f
Maria <maria.mihaila.17@gmail.com>
2021-09-14 12:58:39 +0300

    Remove unnecessary log

--- src/compute-balance.js
+++ src/compute-balance.js
@@ -3,7 +3,6 @@ export const computeBalance = (transactions) => {
     for (let i = 0; i < transactions.length; i++) {
         if (transactions[i].type === 'credit'){
             balance += transactions[i].amount;
-            console.log("balance in for ", balance);
         } else if (transactions[i].type === 'debit'){
             balance -= transactions[i].amount;
         }
