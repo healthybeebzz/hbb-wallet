8400e0ff8971457e70bf423c75b436ce457834a6
Maria <maria.mihaila.17@gmail.com>
2021-10-15 17:47:03 +0300

    Fix credit integration test

--- src/credit.int.test.ts
+++ src/credit.int.test.ts
@@ -1,17 +1,23 @@
 import {expect} from 'chai';
-import * as request from 'supertest';
+import {default as axios} from 'axios';
 import {createWebServer} from "./create-web-server";
+import {insertTransaction, OperationType} from "./transactions";
+import {connectToDb} from "./db-connection";
 
 
 describe('/balance/credit', () => {
     let port: number;
     let server: { stop: () => Promise<unknown>, port: number, start: () => Promise<unknown> };
 
+    let pool= connectToDb();
+
     before(async () => {
         server = createWebServer();
         port = server.port;
 
         await server.start();
+        await insertTransaction(pool, 987, 500, '123', OperationType.CREDIT);
+
     });
 
     after(async () => {
@@ -20,17 +26,17 @@ describe('/balance/credit', () => {
 
     it('given existing balance > when calling POST /balance/credit > should return valid response', async () => {
         const payload = {
-            amount: 12,
-            userId: 5,
-            referenceId: 2323
+            amount: 1000,
+            userId: 987,
+            referenceId: '132'
         };
 
-        const response = await request(`http://localhost:${port}`).post('/balance/credit').send(payload);
+        const response = await axios.post(`http://localhost:${port}/balance/credit`, payload);
 
         expect(response.status).to.be.equal(200);
-        expect(response.body).to.be.deep.equal({
-            userId: 5,
-            balance: 100
+        expect(response.data).to.be.deep.equal({
+            userId: 987,
+            balance: 1500
         });
     });
 });
